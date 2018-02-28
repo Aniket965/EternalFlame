92ee220b1d5af40df42985263986eeb5ee3e0dc2
Aniket965 <Aniket965.as@gmail.com>
2018-02-28 18:12:56 +0530

    Added Version 1.0.0🔥

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# EternalFlame
+# EternalFlame 🔥🔥
 Give life 😇To Dead 💀Terminal with EternalFlame 🔥
+## ⚙️ Installation
+
+```swift
+import PackageDescription
+
+let package = Package(
+    name: "MyPackage",
+    dependencies: [
+        .package(url: "http://github.com/aniket965/EternalFlame.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "MyPackage",
+            dependencies: ["EternalFlame"]
+        )
+    ]
+)
+```
+## 📗 Usage
+```swift
+import EternalFlame
+
+let flame = EternalFlame(frames:["🌎 ","🌏 ","🌍 "],delay:100000)
+flame.giveLife()
+sleep(3)
+flame.takeLife()
+
+```
--- Sources/EternalFlame/EternalFlame.swift
+++ Sources/EternalFlame/EternalFlame.swift
@@ -1,6 +1,33 @@
-import Darwin
+import Foundation
 import Eraser
 
-struct EternalFlame {
-    var text = "🔥"
+public class EternalFlame {
+    var frames:[String];
+    var delay:UInt32;
+    var isRunnig:Bool = false;
+    let queue = DispatchQueue(label: "EternalFlame")
+    public init(frames:[String],delay:UInt32) {
+        self.frames = frames
+        self.delay = delay
+    }
+
+    // starts the animations
+    public func giveLife() {
+        self.isRunnig = true
+        queue.async {
+            while self.isRunnig {
+                for frame in self.frames {
+                    UpdateCLI(msg: "\(frame)", delay: self.delay)
+                }
+            }
+        }
+    }
+
+
+
+    // Stops the Animation
+    public func takeLife() {
+        self.isRunnig = false
+    }
+
 }
