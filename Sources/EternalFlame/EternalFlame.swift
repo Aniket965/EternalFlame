16e7c002f8774c910f68cf752a20cb9d7d82efcc
Aniket965 <Aniket965.as@gmail.com>
2018-03-01 08:56:44 +0530

    Added Message And animation Type support

--- Sources/EternalFlame/EternalFlame.swift
+++ Sources/EternalFlame/EternalFlame.swift
@@ -4,11 +4,17 @@ import Eraser
 public class EternalFlame {
     var frames:[String];
     var delay:UInt32;
+    var msg:String;
     var isRunnig:Bool = false;
+    var type:Flametype;
     let queue = DispatchQueue(label: "EternalFlame")
-    public init(frames:[String],delay:UInt32) {
+    
+    public init(frames:[String],delay:UInt32,msg:String,type:Flametype) {
         self.frames = frames
+        self.msg = msg
         self.delay = delay
+        self.type = type
+        
     }
 
     // starts the animations
@@ -17,9 +23,16 @@ public class EternalFlame {
         queue.async {
             while self.isRunnig {
                 for (index,frame) in  self.frames.enumerated() {
-                    let fractionCompleted :Float32 = Float32((index + 1)) / Float32(self.frames.count)
-                    let d = self.easeInOut(t: fractionCompleted) * Float32(self.delay)
-                    UpdateCLI(msg: "\(frame)", delay: UInt32(d) )
+                    var d:Float32
+                    switch self.type {
+                    case .ease:
+                        let fractionCompleted :Float32 = Float32((index + 1)) / Float32(self.frames.count)
+                        d = self.easeInOut(t: fractionCompleted) * Float32(self.delay)
+                    case .linear:
+                        d = Float32(self.delay)
+                    }
+
+                    UpdateCLI(msg: "\(frame) \(self.msg)", delay: UInt32(d) )
                 }
             }
         }
@@ -36,8 +49,11 @@ public class EternalFlame {
     private func easeInOut(t:Float32) -> Float32 {
        return t < 0.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
     }
+    
 
 }
+
+// Animation Type
 public enum Flametype {
     case ease
     case linear
